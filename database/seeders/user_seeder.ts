f1910d905365076b739dc2d7532e6fbc5827289f
Agus Siswanto <agus.siswanto@erajaya.com>
2024-08-04 19:34:33 +0700

    adonisjs create project , migration, model, seeder

---
+++ database/seeders/user_seeder.ts
@@ -0,0 +1,20 @@
+import User from '#models/user';
+import hash from '@adonisjs/core/services/hash'
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+
+export default class extends BaseSeeder {
+  async run() {
+    await User.createMany([
+      {
+        fullName: 'Agus Siswanto',
+        email: 'agus.siswanto@erajaya.com',
+        password: await hash.use('scrypt').make('secret'),
+      },
+      {
+        fullName: 'Dealitha Winata',
+        email: 'dealitha.winata@erajaya.com',
+        password: await hash.use('scrypt').make('secret'),
+      },
+    ]);
+  }
+}
