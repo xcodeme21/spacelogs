6639b2010327f250b13bebbebb9caebda4adc6d8
Agus Siswanto <agus.siswanto@erajaya.com>
2024-08-05 18:11:58 +0700

    add time

--- resources/js/home.js
+++ resources/js/home.js
@@ -88,5 +88,22 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   
     fetchData();
+    getDateTime();
   });
+
+
+  async function getDateTime() {
+    function updateTime(){
+        var str = "";
+
+        var days = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+        var months = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+
+        var now = new Date();
+
+        str += days[now.getDay()] + ", " + now.getDate() + " " + months[now.getMonth()] + " " + now.getFullYear() + " " + now.getHours() +":" + now.getMinutes() + ":" + now.getSeconds();
+        document.getElementById("date_time_now").innerHTML = str;
+    }
+    setInterval(updateTime, 1000);
+  }
   
