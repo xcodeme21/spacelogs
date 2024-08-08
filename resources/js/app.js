81e9dfe2fd3d6b34cac539dcfd37d7a7c10945d3
Agus Siswanto <agus.siswanto@erajaya.com>
2024-08-08 11:14:48 +0700

    fixing menu

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
-console.log('Hello World')
+document.addEventListener('DOMContentLoaded', function () {
+    const menuButton = document.getElementById('mobile-menu-button')
+  const mobileMenu = document.getElementById('mobile-menu')
+  const openIcon = menuButton.querySelector('svg:nth-of-type(1)')
+  const closeIcon = menuButton.querySelector('svg:nth-of-type(2)')
+
+  menuButton.addEventListener('click', function () {
+    mobileMenu.classList.toggle('hidden')
+    openIcon.classList.toggle('hidden')
+    closeIcon.classList.toggle('hidden')
+  })
+
+  const button = document.getElementById('user-menu-button')
+  const dropdownMenu = document.getElementById('dropdown-menu')
+
+  button.addEventListener('click', function () {
+    dropdownMenu.classList.toggle('hidden')
+  })
+});
